b4f4e206db9fa046c7598300ee8c6f3eb77e3455
LeoNoli <nollizleo@gmail.com>
2023-05-02 21:28:28 -0300

    Operadores e funções

---
+++ funcoes.js
@@ -0,0 +1,22 @@
+//let x = '';
+//console.log(x)
+//x= 'oi';
+
+//1) declara a função
+                      //string
+function imprimeTexto(texto){
+    console.log(texto)
+}
+
+//2) executa a função (1 ou mais vezes)
+
+imprimeTexto('oi');
+imprimeTexto('outro texto');
+
+// tres formas de escrever funções
+
+function soma (){
+    return 3 + 3;
+}
+
+console.log(soma());
---
+++ parametros.js
@@ -0,0 +1,22 @@
+//parametros de funções
+
+
+               //2   //2
+//function soma (num1, num2){
+    //return num1+num2;
+//}
+
+console.log(soma(2,2));
+console.log(soma(500,597));
+console.log(soma(-500,226));
+
+function soma (num1, num2){
+    return num1+num2;
+}
+
+function multiplica (nm1=1,nm2=1){
+    return nm1*nm2;
+}
+
+console.log(multiplica(soma(4,5)));
+
